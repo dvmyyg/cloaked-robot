41d311bb57cb83f06e4bd675c7a5401b198dac87
David Vuillardot <davidvuillardot@gmail.com>
2014-02-11 21:25:48 +0100

    1ere sauvegarde

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MimoApp::Application.config.secret_key_base = 'c7a1f819ff8d76e85b3eef8fbfdd8caab4c7a39b309a2b62c46eafa09d701fb732470145653fc3ebd523e7ed1fa04273c6cd7c7fffdcd349ad98de069be974ab'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MimoApp::Application.config.session_store :cookie_store, key: '_mimo_app_session'

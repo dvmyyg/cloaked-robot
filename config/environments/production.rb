d74c2d2c325d331d2c3b94d4e8519d32d0e68279
David Vuillardot <davidvuillardot@gmail.com>
2014-02-13 16:55:24 +0100

    implemented devise for users

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,10 @@ MimoApp::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+
+  # required for heroku
+  # note to set this to the actual host
+  config.action_mailer.default_url_options = { :host => 'mimoapp.herokuapp.com'}
+
 end
